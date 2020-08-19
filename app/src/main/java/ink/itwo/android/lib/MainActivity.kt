6ed271330e6267f03745c8bad21c87ecd51cc3a5
wangtaian <dadianwang@gmail.com>
2020-08-19 18:04:21 +0800

    1

--- app/src/main/java/ink/itwo/android/lib/MainActivity.kt
+++ app/src/main/java/ink/itwo/android/lib/MainActivity.kt
@@ -2,11 +2,10 @@ package ink.itwo.android.lib
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ink.itwo.android.common.ktx.log
 import ink.itwo.android.coroutines.permissions.requestCoroutinesPermissions
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,8 +18,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun aa() {
-        CoroutineScope(Dispatchers.Main).launch {
-            val result = requestCoroutinesPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        GlobalScope.launch {
+           var result= withContext(context = Dispatchers.Main){
+                requestCoroutinesPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+            result.log()
+            result.log()
         }
     }
 
